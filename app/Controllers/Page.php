19fa1860335643cc086b7b4edb5544c9a770d045
aithrajbouty <aithrabouty@gmail.com>
2020-11-10 21:06:32 +0800

    tes

---
+++ app/Controllers/Page.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+class Page extends BaseController
+{
+	public function about(){
+		echo "Halaman About";
+	}
+	
+	public function contact(){
+		echo "Halaman Contact";
+	}
+	
+	public function faqs(){
+		echo "Halaman Faqs";
+	}
+}
