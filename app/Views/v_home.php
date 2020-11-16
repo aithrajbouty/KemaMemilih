1fc8972527474ff24fd6afd6d63a71ef2e689747
aithrajbouty <aithrabouty@gmail.com>
2020-11-16 21:14:36 +0800

    templates

--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -1,50 +1,48 @@
+<body class="home-body">
+<div id="theCarousel" class="carousel slide home-carousel" data-ride="carousel">
 
-	
-	<body class="home-body">
-        <div id="theCarousel" class="carousel slide home-carousel" data-ride="carousel">
+    <ol class="carousel-indicators">
+    <li data-target="#theCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#theCarousel" data-slide-to="1"></li>
+    <li data-target="#theCarousel" data-slide-to="2"></li>
+    </ol>
 
-            <ol class="carousel-indicators">
-            <li data-target="#theCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#theCarousel" data-slide-to="1"></li>
-            <li data-target="#theCarousel" data-slide-to="2"></li>
-            </ol>
-
-            <div class="carousel-inner text-center">
-                <div class="item active">
-                    <div class="slide1"></div>
-                    <div>
-                        <h1 class="carousel-caption">Kema Memilih</h1>
-                    </div>
-                </div>
-                
-                <div class="item">
-                    <div class="slide2"></div>
-                </div>
-                
-                <div class="item">
-                    <div class="slide3"></div>
-                </div>
+    <div class="carousel-inner text-center">
+        <div class="item active">
+            <div class="slide1"></div>
+            <div>
+                <h1 class="carousel-caption">Kema Memilih</h1>
             </div>
-
-            <a class="left carousel-control" href="#theCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-            </a>
-
-            <a class="right carousel-control" href="#theCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-            </a>
         </div>
         
-        <div class="jumbotron jumbotron-fluid jumbo-home">
-            <div class="container">
-                <div class="row">   
-                    <div class="col-sm-6 home-caption">
-                        <p>Kamu Kema Unpad?</p>
-                    </div>
-                    <div class="col-sm-6 text-right">
-                        <button class="btn home-btn" onclick="location.href='Login/index'">Login Disini</button>
-                    </div>
-                </div> 
-            </div>
+        <div class="item">
+            <div class="slide2"></div>
         </div>
+        
+        <div class="item">
+            <div class="slide3"></div>
+        </div>
+    </div>
+
+    <a class="left carousel-control" href="#theCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+
+    <a class="right carousel-control" href="#theCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+</div>
+
+<div class="jumbotron jumbotron-fluid jumbo-home">
+    <div class="container">
+        <div class="row">   
+            <div class="col-sm-6 home-caption">
+                <p>Kamu Kema Unpad?</p>
+            </div>
+            <div class="col-sm-6 text-right">
+                <button class="btn home-btn" onclick="location.href='Login/index'">Login Disini</button>
+            </div>
+        </div> 
+    </div>
+</div>
 
