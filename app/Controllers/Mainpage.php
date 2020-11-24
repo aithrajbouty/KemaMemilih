bd3c8f5cca676ca9c0b51247fe9dec4287f7833e
aithrajbouty <aithrabouty@gmail.com>
2020-11-24 22:26:59 +0800

    navbar lagi

--- app/Controllers/Mainpage.php
+++ app/Controllers/Mainpage.php
@@ -5,11 +5,12 @@ class Mainpage extends BaseController
 	public function index()
 	{
 		$data = [
-			'title' => 'Mainpage - Kema Memilih'
+			'title' => 'Mainpage - Kema Memilih',
+			'page' => 'mainpage'
 		];
 		echo view('templates/header', $data);
 		echo view('templates/navbar1');
-		echo view('templates/navbar2');
+		echo view('templates/navbar2', $data);
 		echo view('v_mainpage');
 		echo view('templates/footer');
 	}
--- app/Views/templates/navbar1.php
+++ app/Views/templates/navbar1.php
@@ -1,29 +1,10 @@
-<!--<body>
-<nav class="navbar navbar1">
-    <div class="container-fluid">
-        <div class="col-sm-4">
-            <ul class="nav navbar-nav navbar-left">
-                <li><a href="#"><span class="glyphicon glyphicon-arrow-left"></span></a></li>
-            </ul>
-        </div>
-        <div class="col-sm-4">
-            <ul class="nav navbar-nav navbar-center">
-                <a class="navbar-brand" href="mainpage/index">Kema Memilih</a>
-            </ul>
-        </div>
-        <div class="col-sm-4">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="profile/index"><span class="glyphicon glyphicon-user"></span></a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
-</body>
--->
 <body>
 <div class="navbar navbar1">
     <a href="#" style="text-align: left"><span class="glyphicon glyphicon-arrow-left"></span></a>
     <a class="navbar-brand" href="mainpage/index" style="text-align: center">Kema Memilih</a>
     <a href="profile/index" style="text-align: right"><span class="glyphicon glyphicon-user"></span></a>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
