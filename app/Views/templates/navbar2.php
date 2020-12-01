5f5b7b04e1b216ee8ad47198f184472295c3363f
aithrajbouty <aithrabouty@gmail.com>
2020-12-01 20:38:59 +0800

    hubungin page statis

--- app/Views/templates/navbar2.php
+++ app/Views/templates/navbar2.php
@@ -1,7 +1,7 @@
 <body>
 <div class="navbar navbar2">
     <a class="<?php if($page=='fakultas'){echo 'active';} ?>" href="<?= base_url('MainpageFak')?>">Fakultas</a>
-    <a class="<?php if($page=='statis'){echo 'active';} ?>" href=#>Hasil Statistika</a>
+    <a class="<?php if($page=='statistik'){echo 'active';} ?>" href="<?= base_url('Statistik')?>">Hasil Statistika</a>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
