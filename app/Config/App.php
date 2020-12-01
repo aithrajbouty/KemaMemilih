8b9b8a6f8ca3abbc6d6ab5862de0fdb76f456a92
aithrajbouty <aithrabouty@gmail.com>
2020-12-01 18:38:07 +0800

    hubungin halaman profil

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost:9080/KemaMemilih/public/';
+	public $baseURL = 'http://localhost/KemaMemilih/public/';
 
 	/*
 	|--------------------------------------------------------------------------
